6bb1ce9b167fbaccff5fd8aa7cce3f0e1ccbdc4b
Olga  Chupp <olga.chupp@gmail.com>
2019-10-28 06:08:27 +0000

    Automatically backed up by Learn

--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -5,7 +5,7 @@ describe "nested hash" do
   describe "hopper" do
     it "operates on the programmer_hash and returns the value of the :grace_hopper key" do
       expect(hopper).to eq({:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]})
-
+#rspec is requiring a nested hash to be defined
     end
   end
 
